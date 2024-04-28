a0f64c16436cfd67f778866c96d5c3050642a144
Danil <dkorostelev1308@gmail.com>
2024-04-28 16:24:49 +0700

    Добавлен комментарий к функции isPrime

--- Inginirium/Inginirium.cpp
+++ Inginirium/Inginirium.cpp
@@ -2,29 +2,30 @@
 
 using namespace std;
 
+// Функция для проверки, является ли число простым
 bool isPrime(int n) {
-	if (n <= 1) {
-		return false;
-	}
-	for (int i = 2; i * i <= n; ++i) {
-		if (n % i == 0) {
-			return false;
-		}
-	}
-	return true;
+    if (n <= 1) {
+        return false;
+    }
+    for (int i = 2; i * i <= n; ++i) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
 }
 
 int main() {
-	int num;
-	cout « "Введите целое число: ";
-	cin » num;
+    int num;
+    cout << "Введите целое число: ";
+    cin >> num;
 
-	if (isPrime(num)) {
-		cout « num « " - простое число" « endl;
-	}
-	else {
-		cout « num « " - составное число" « endl;
-	}
+    if (isPrime(num)) {
+        cout << num << " - простое число" << endl;
+    }
+    else {
+        cout << num << " - составное число" << endl;
+    }
 
-	return 0;
+    return 0;
 }
