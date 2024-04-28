ecadcbfed912b07d5fe14fde815e7749e833db9d
Danil <dkorostelev1308@gmail.com>
2024-04-28 16:21:04 +0700

    Первоначальная версия программы

---
+++ Inginirium/Inginirium.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+bool isPrime(int n) {
+	if (n <= 1) {
+		return false;
+	}
+	for (int i = 2; i * i <= n; ++i) {
+		if (n % i == 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+int main() {
+	int num;
+	cout « "Введите целое число: ";
+	cin » num;
+
+	if (isPrime(num)) {
+		cout « num « " - простое число" « endl;
+	}
+	else {
+		cout « num « " - составное число" « endl;
+	}
+
+	return 0;
+}
